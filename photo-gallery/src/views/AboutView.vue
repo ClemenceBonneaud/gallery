9013eea5bc4acb3e92fc0af27b5e6d2f4e97a43d
Clemence <clemence.bonneaud@gmail.com>
2025-01-23 00:54:56 +0100

    initial commit

---
+++ photo-gallery/src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1 class="text-xl text-red-500">ABOUT</h1>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ photo-gallery/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <p class="text-xs text-red-500">Home</p>
+  </main>
+</template>
+
+<script setup>
+// Si vous n'avez pas de script ici, vous pouvez le laisser vide ou ajouter de la logique Vue.
+</script>
