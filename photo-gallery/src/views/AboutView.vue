ff9c7b96ef9a74f8adb766f879a0d61473644f0d
Clemence <clemence.bonneaud@gmail.com>
2025-02-06 12:04:04 +0100

    cleaning files

--- photo-gallery/src/views/AboutView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-    <h1 class="text-xl text-red-500">ABOUT</h1>
-  </div>
-</template>
-
-<style>
-</style>
---
+++ photo-gallery/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template class="flex flex-col justify-center">
+  <div>
+    <p>test</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const colors = ['g8', 'light', '#692A90', '#29135D', '#04D1CB', '#6CD2C3', '#ADCD92']
+</script>
+
+<style lang="scss">
+
+</style>
